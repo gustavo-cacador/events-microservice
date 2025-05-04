6a8b4e7ae95a755baec39e3d6c50eed41b3ff5b8
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-05-04 13:50:58 -0300

    Initial commit

---
+++ events-microservice/src/test/java/com/gustavo/events_microservice/EventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.events_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
